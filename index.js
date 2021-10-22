bcd2d2a71de237af8185516413650c10ddc79a70
Chiradeep-Banik <chiradeep13@gmail.com>
2021-10-23 04:10:20 +0530

    Built the project

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-var token = process.env['BOT_TOKEN'];
-console.log(token);
